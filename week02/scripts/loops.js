0572ed9f9bb008d38a169b59e3fc2bb6959fa000
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-14 12:06:23 -0300

    trying js loop

---
+++ week02/scripts/loops.js
@@ -0,0 +1,25 @@
+myInfo = {
+    name: "Brother T",
+    photo: "images/photo.jpg",
+    favoriteFoods: ["Fettucini", "Steak", "Chicken", "Shrimp", "Baked Potato"],
+    hobbies: ["Reading", "Fishing", "Camping"],
+    placesLived: [
+      {
+        place: "Rexburg, ID",
+        length: "5 years",
+      },
+      {
+        place: "Ammon, ID",
+        length: "3 years",
+      },
+      {
+        place: "Sandy, UT",
+        length: "1 year",
+      },
+    ],
+  };
+
+myInfo.favoriteFoods.array.forEach(function(element) {
+   let element = document.createElement("li");
+    document.querySelector("#favorite-foods").appendChild(element)
+});
