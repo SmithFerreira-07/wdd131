51325535272a92e08fb2a56c563ee1251e1215d2
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-17 20:43:14 -0300

    working around X button and why nav-links are showing automatically when the screen is on phone size

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -2,3 +2,20 @@ const year = new Date().getFullYear();
 document.getElementById("currentyear").textContent = year;
 const lastModification = document.lastModified;
 document.getElementById("modification").textContent = lastModification;
+
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+const xButton = document.getElementById('x-button');
+
+
+
+hamburger.addEventListener('click', () => {
+  if (navLinks.style.display === "flex") {
+    navLinks.style.display = "none";
+  } else {
+    navLinks.style.display = "flex";
+    
+    
+  }
+});
