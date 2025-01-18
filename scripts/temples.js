f060df9d0c4b55a62bbe6cc40ee20ef887d9a641
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-18 14:09:58 -0300

    taking out some obsolete things

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,7 +6,7 @@ document.getElementById("modification").textContent = lastModification;
 
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
-const xButton = document.getElementById('x-button');
+
 
 
   
