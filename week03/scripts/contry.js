18ad06b0bafe57b5f054377046fee601f4502fc0
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-27 16:08:02 -0300

    added golden lion media query, added picture tag and other adjustments

--- week03/scripts/contry.js
+++ week03/scripts/contry.js
@@ -2,3 +2,22 @@ const year = new Date().getFullYear();
 document.getElementById("currentyear").textContent = year;
 const lastModification = document.lastModified;
 document.getElementById("modification").textContent = lastModification;
+
+
+const A = 13.12;
+const B = 0.6215;
+const C = 11.37;
+const D = 0.3965;
+
+function calculateWindChill(temp, windSpeed) {
+    if (temp > 10 || windSpeed <= 4.8) return null;
+  
+    const result = A + (B * temp) - (C * Math.pow(windSpeed, 0.16)) + (D * temp * Math.pow(windSpeed, 0.16));
+    
+   
+    return `${result.toFixed(1)}°C`;
+  }
+
+const windChill = calculateWindChill(10, 18)
+
+document.getElementById("windChillValue").textContent = windChill;
